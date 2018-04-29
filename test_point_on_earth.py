cf35ecd4fb34571e31f8e7964dc4ebb4ac83dae5
Alex Vykaliuk <alekzvik@gmail.com>
2018-04-29 14:55:30 +0300

    Fix lat/long order in a test.

--- test_point_on_earth.py
+++ test_point_on_earth.py
@@ -6,5 +6,5 @@ from point_on_earth import generate_land_coordinates, get_land_polygons
 def test_point_on_land():
     coordinates = generate_land_coordinates()
     land = get_land_polygons()
-    geo_point = Point(coordinates['latitude'], coordinates['longitude'])
+    geo_point = Point(coordinates['longitude'], coordinates['latitude'])
     assert land.contains(geo_point)
